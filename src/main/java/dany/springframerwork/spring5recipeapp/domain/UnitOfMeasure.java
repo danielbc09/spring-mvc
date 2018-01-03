823f1e8dc3d67739065da2ff0c5f0184bf47a72a
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-01-03 16:41:38 -0500

    Database Initialization with Spring

--- src/main/java/dany/springframerwork/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/dany/springframerwork/spring5recipeapp/domain/UnitOfMeasure.java
@@ -15,7 +15,7 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String uom;
+    private String description;
 
     public Long getId() {
         return id;
@@ -26,10 +26,10 @@ public class UnitOfMeasure {
     }
 
     public String getUom() {
-        return uom;
+        return description;
     }
 
     public void setUom(String uom) {
-        this.uom = uom;
+        this.description = uom;
     }
 }

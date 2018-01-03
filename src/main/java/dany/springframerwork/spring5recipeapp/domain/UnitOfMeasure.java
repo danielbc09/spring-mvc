7d3a66c923fb2e9561fd4b14c21099cae514625f
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-01-03 15:36:04 -0500

    One to many , and OneToOne assigment realtionship

---
+++ src/main/java/dany/springframerwork/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,35 @@
+package dany.springframerwork.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by bautisj on 1/3/2018.
+ */
+@Entity
+public class UnitOfMesure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+}

21924a7bc772713547436c19ad16169e2d2f0217
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-01-09 17:00:00 -0500

    Adding Unit Test

---
+++ src/test/java/dany/springframerwork/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,34 @@
+package dany.springframerwork.spring5recipeapp.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bautisj on 1/9/2018.
+ */
+public class CategoryTest {
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4l;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+    }
+
+}

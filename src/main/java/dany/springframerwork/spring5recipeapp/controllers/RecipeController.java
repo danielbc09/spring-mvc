9bab8579edc0084306b31724ed833b033e28f567
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-01-23 11:11:41 -0500

    Create a recipe , adding html show recipe, adding controller method

--- src/main/java/dany/springframerwork/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/dany/springframerwork/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,11 @@
 package dany.springframerwork.spring5recipeapp.controllers;
 
+import dany.springframerwork.spring5recipeapp.commands.RecipeCommand;
 import dany.springframerwork.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by bautisj on 1/10/2018.
@@ -24,7 +24,22 @@ public class RecipeController {
 
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
+
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
+
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String SaveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand saveCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" +  saveCommand.getId();
     }
 }

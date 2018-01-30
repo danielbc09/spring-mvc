d815dd049af0d17407a916b3498d5ba58c1aec91
Your Name <you@example.com>
2018-01-29 21:12:52 -0500

    Fixing the context IngredientController error

--- src/main/java/dany/springframerwork/spring5recipeapp/services/UnitOfMeasureService.java
+++ src/main/java/dany/springframerwork/spring5recipeapp/services/UnitOfMeasureService.java
@@ -1,12 +1,14 @@
 package dany.springframerwork.spring5recipeapp.services;
 
 import dany.springframerwork.spring5recipeapp.commands.UnitOfMeasureCommand;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 /**
  * Created by bautisj on 1/29/2018.
  */
+@Service
 public interface UnitOfMeasureService {
 
     Set<UnitOfMeasureCommand> listAllUnitOfMeasure();
--- src/main/java/dany/springframerwork/spring5recipeapp/services/UnitOfMeasureServiceImpl.java
+++ src/main/java/dany/springframerwork/spring5recipeapp/services/UnitOfMeasureServiceImpl.java
@@ -3,6 +3,7 @@ package dany.springframerwork.spring5recipeapp.services;
 import dany.springframerwork.spring5recipeapp.commands.UnitOfMeasureCommand;
 import dany.springframerwork.spring5recipeapp.converters.UnitOfMeasureToUnitOfMeasureCommand;
 import dany.springframerwork.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -11,6 +12,7 @@ import java.util.stream.StreamSupport;
 /**
  * Created by bautisj on 1/29/2018.
  */
+@Service
 public class UnitOfMeasureServiceImpl implements UnitOfMeasureService{
 
     private final UnitOfMeasureRepository unitOfMeasureRepository;

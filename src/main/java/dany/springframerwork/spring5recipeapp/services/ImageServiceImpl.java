9a02e17e8238c76b974e5103baae3c271dd0191f
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-02-01 14:14:07 -0500

    Persisting images DB

--- src/main/java/dany/springframerwork/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/dany/springframerwork/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,8 +1,15 @@
 package dany.springframerwork.spring5recipeapp.services;
 
+import dany.springframerwork.spring5recipeapp.domain.Recipe;
+import dany.springframerwork.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import sun.rmi.runtime.Log;
+
+import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Created by bautisj on 2/1/2018.
@@ -11,8 +18,35 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImageServiceImpl implements ImageService{
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received File");
+        try{
+
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+
+        }catch (IOException e){
+            //TO-DO Exceptiobn Handler
+            log.error("Error Ocuried", e);
+            e.printStackTrace();
+        }
     }
 }

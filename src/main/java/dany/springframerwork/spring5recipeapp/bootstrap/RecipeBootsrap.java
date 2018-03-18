5877614213c1eff0401fd934be25beb833625c45
Your Name <you@example.com>
2018-03-17 22:32:58 -0500

    Refactoring database initialization for MySQL

--- src/main/java/dany/springframerwork/spring5recipeapp/bootstrap/RecipeBootsrap.java
+++ src/main/java/dany/springframerwork/spring5recipeapp/bootstrap/RecipeBootsrap.java
@@ -6,8 +6,10 @@ import dany.springframerwork.spring5recipeapp.repositories.RecipeRepository;
 import dany.springframerwork.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -15,20 +17,22 @@ import java.util.List;
 import java.util.Optional;
 
 @Slf4j
-@Component
-public class RecipeBoostrap implements ApplicationListener<ContextRefreshedEvent>{
+@Component()
+@Profile("default")
+public class RecipeBootsrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private final CategoryRepository categoryRepository;
     private final RecipeRepository recipeRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public RecipeBoostrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+    public RecipeBootsrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.categoryRepository = categoryRepository;
         this.recipeRepository = recipeRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipies());
         log.debug("Loading bootstrap data");
